657212321d3f4fd306bb6079e8f99c2e8624a211
ryanNS3 <ryansantosn333@gmail.com>
2026-06-02 20:08:06 -0300

    Initial commit

---
+++ src/main/java/com/example/floricultura/FloriculturaApplication.java
@@ -0,0 +1,13 @@
+package com.example.floricultura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloriculturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FloriculturaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/floricultura/FloriculturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.floricultura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloriculturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
