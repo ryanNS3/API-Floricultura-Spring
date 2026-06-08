1bd006b45e7e3360ccb85f481829e19c552b01ae
ryanNS3 <ryansantosn333@gmail.com>
2026-06-07 21:54:49 -0300

    Fix: Ajustando o modelo dos produtos

--- src/main/java/com/example/floricultura/database/model/Produto.java
+++ src/main/java/com/example/floricultura/database/model/Produto.java
@@ -1,23 +1,28 @@
 package com.example.floricultura.database.model;
 
 // IMPORTANTE: Os imports corretos vêm do jakarta.persistence
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.Data; // Se quiser usar o Lombok para gerar getters/setters automaticamente
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
-@Table(name = "produtos") // Define o nome da tabela que será criada no SQL Server
+@Table(name = "Produto") // Define o nome da tabela que será criada no SQL Server
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
 public class Produto {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // Auto-incremento para o SQL Server
-    private Long id;
-
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+    @Column(nullable = false)
     private String nome;
+    @Column(nullable = false)
     private Double preco;
-    private Integer quantidade;
+    @Column(nullable = false)
+    private Integer qtd_estoque;
+    @Column(nullable = false)
+    private String imagem;
 }
