ecfcd86bf30edb0408475a1879fd6ead4b5794c7
ryanNS3 <ryansantosn333@gmail.com>
2026-06-07 21:56:00 -0300

    feat: Adicionando controller

--- src/main/java/com/example/floricultura/controller/Produtos.java
+++ src/main/java/com/example/floricultura/controller/Produtos.java
@@ -1,14 +1,37 @@
 package com.example.floricultura.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.floricultura.database.model.Produto;
+import com.example.floricultura.service.ProdutoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/produto")
+@RequiredArgsConstructor
 public class Produtos {
+
+    private final ProdutoService produtoService;
+
     @GetMapping
-    public String Hello(){
-        return "hello";
+    @ResponseStatus(HttpStatus.OK)
+    public List<Produto> findALL(){
+        return produtoService.findAll();
+    }
+
+    @PostMapping
+    @RequestMapping("/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Produto addProduct(@RequestBody Produto produto){
+        return produtoService.createProduct(produto);
+    }
+
+    @DeleteMapping
+    @RequestMapping("/delete")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public String deleteProduct(@PathVariable String id){
+        return produtoService.deleteProductById(id);
     }
 }
